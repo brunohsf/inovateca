80be4b47cb7dfe37b269376b15aa28d3314ddd1a
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-11-18 21:06:32 -0300

    implementa repositories

--- src/main/java/com/inovateca/InovaTeca/repository/AutorRepository.java
+++ src/main/java/com/inovateca/InovaTeca/repository/AutorRepository.java
@@ -1,4 +1,20 @@
 package com.inovateca.InovaTeca.repository;
 
-public class GeneroRepository {
+import com.inovateca.InovaTeca.model.AutorModel;
+import com.inovateca.InovaTeca.model.LivroModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AutorRepository extends JpaRepository<AutorModel, Long> {
+
+    public Optional<AutorModel> findById(Long id);
+
+    List<AutorModel> findAll();
+
+    Page<AutorModel> findAll(Pageable pageable);
+
 }
